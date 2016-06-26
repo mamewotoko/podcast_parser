047480c6d15c68a61a0e89f400eda9aadbd3854d
Takashi Masuyama <mamewotoko@gmail.com>
2016-06-26 13:00:51 +0900

    add pubdate comparator

--- src/com/mamewo/lib/podcast_parser/EpisodeInfo.java
+++ src/com/mamewo/lib/podcast_parser/EpisodeInfo.java
@@ -5,11 +5,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Comparator;
 import android.util.Log;
 
 public class EpisodeInfo
 	implements Serializable
 {
+    //read
 	static final String DATE_PATTERN = "EEE, dd MMM yyyy HH:mm:ss Z";
 	static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_PATTERN, Locale.US);
 
@@ -43,7 +45,10 @@ public class EpisodeInfo
 		
 	public String getPubdateString(){
 		if(null != pubdateobj_) {
-			return pubdateobj_.toLocaleString();
+			//return pubdateobj_.toLocaleString();
+            synchronized(DATE_FORMAT){
+                return DATE_FORMAT.format(pubdateobj_);
+            }
 		}
 		return pubdate_;
 	}
@@ -51,4 +56,39 @@ public class EpisodeInfo
 	public boolean equalEpisode(EpisodeInfo other){
 		return url_.equals(other.url_) && pubdate_.equals(other.pubdate_);
 	}
+
+    static
+    public class PubdateComparator
+        implements Serializable,
+                   Comparator<EpisodeInfo>                   
+    {
+        @Override
+        public int compare(EpisodeInfo o1, EpisodeInfo o2){
+            if(null == o1.pubdateobj_ && null == o1.pubdateobj_){
+                //use occurence order or compare date string
+                return 0;
+            }
+            if(null == o1.pubdateobj_){
+                return -1;
+            }
+            if(null == o2.pubdateobj_){
+                return 1;
+            }
+            return o1.pubdateobj_.compareTo(o2.pubdateobj_);
+        }
+    }
+    
+    // static
+    // public OccurenceOrderComparator
+    //     extends Compator<EpisodeInfo>
+    //     implements Serializable
+    // {
+    //     @Override
+    //     public int compare(EpisodeInfo o1, EpisodeInfo o2){
+    //         if(o1.index_ != o2.index_){
+    //             return o1.index_ - o2.index_;
+    //         }
+    //         //
+    //     }
+    // }
 }
